8fe22782ccd7a3819816e1bd396a09d49cbdd2b1
Ashwin vemula <ashwin.vemula@cawstudios.com>
2023-09-22 00:06:34 +0530

    NA

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,6 +6,7 @@
 
 const path = require('path');
 const _ = require('lodash');
+require('events').EventEmitter.defaultMaxListeners = 30; // or a higher number as needed
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions;
